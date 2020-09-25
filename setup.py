aab484c5816697988d8ea6fc324d46487e677580
james <james.pavur@cs.ox.ac.uk>
2020-09-25 15:15:27 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='gsextract',
-    version='0.1',
+    version='1.0',
+    author='James Pavur',
+    author_email='james.pavur@cs.ox.ac.uk',
+    description='A tool to parse corrupted recordings of satellite internet traffic in the GSE over DVB-S format into .pcaps',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ssloxford/gsextract",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Security"
+    ],
+    python_required='>=3.7',
     packages=find_packages(),
-    #py_modules=['gsextract'],
     setup_requires=['wheel'],
     install_requires=[
         'kaitaistruct',
